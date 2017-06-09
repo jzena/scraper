c4d1e67f29f583af6962921ecda308ffad5e93d1
Esteban Petrovich <epetrovich@3c.com>
2017-06-09 16:40:13 -0300

    Modified for TFP-723

--- bin/pjscrape.sh
+++ bin/pjscrape.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-phantomjs $(dirname $0)/../pjscrape.js $*;
-prettyjson data.json;
+/Users/epetrovich/node_modules/phantomjs-prebuilt/lib/phantom/bin/phantomjs $(dirname $0)/../pjscrape.js $*;
+/Users/epetrovich/node_modules/phantomjs-prebuilt/lib/phantom/bin/phantomjs data.json;
--- mysites.js
+++ mysites.js
@@ -1,5 +1,6 @@
 pjs.config({
-    allowRepeatUrls: true,
+    //allowRepeatUrls: true,
+    ignoreDuplicates: true,
     log: 'stdout',
     format: 'json',
     writer: 'file',
@@ -7,27 +8,63 @@ pjs.config({
 });
 
 pjs.addSuite({
-    url: 'http://mysites.safelink.cms.stage.3cinteractive.com/',
-    moreUrls: function() {
-        return _pjs.getAnchorUrls('li a, p a, nav a');
+    url: [
+        'http://mysites.myfamilymobile.cms.stage.3cinteractive.com',
+        'http://mysites.net10.cms.stage.3cinteractive.com',
+        'http://mysites.safelink.cms.stage.3cinteractive.com',
+        'http://mysites.simplemobile.cms.stage.3cinteractive.com',
+        'http://mysites.straighttalk.cms.stage.3cinteractive.com',
+        'http://mysites.totalwireless.cms.stage.3cinteractive.com',
+        'http://mysites.tracfone.cms.stage.3cinteractive.com/index'
+    ],
+    moreUrls: function () {
+        var followUrls = [];
+        var unfilteredFollowUrls = _pjs.getAnchorUrls('a', false);
+        for (var i = 0; i < unfilteredFollowUrls.length; i++) {
+            if (unfilteredFollowUrls[i].indexOf('/redir') === -1 && unfilteredFollowUrls[i].indexOf('/r/') === -1) {
+                followUrls.push(unfilteredFollowUrls[i]);
+            }
+        }
+        return followUrls;
     },
     scrapers: [
-      function() {
-        _pjs.state.counter = _pjs.state.counter + 1 || 0;
+        function () {
+            _pjs.state.counter = _pjs.state.counter + 1 || 0;
 
-        var items = [];
-        $('a').each(function() {
-          if ($(this).attr('href') && $(this).attr('href').length && ($(this).attr('href').indexOf('/tfredir') > -1 || $(this).attr('href').indexOf('/redir/') > -1)) {
-            var text = $(this).text().trim().length ? $(this).text().trim() : "This link doesn't have a text, maybe its child is an image";
-            var link = $(this).attr('href');
-            items.push({
-                text: text,
-                link: link,
+            var items = [];
+
+            var isInDestSites = function(href) {
+                var searchDestSites = [
+                    'a.net10.com',
+                    'a.simplemobile.com',
+                    'a.straighttalk.com',
+                    'a.totalwireless.com',
+                    't.tracfone.com'
+                ];
+
+                var result = false;
+
+                $.each(searchDestSites, function(key, destSite) {
+                    result = result || href.indexOf(destSite) !== -1;
+                });
+
+                return result;
+            };
+
+            $('a').each(function () {
+                var $this = $(this);
+                var href = $this.attr('href');
+
+                if (href && href.length && isInDestSites(href)) {
+                    var text = $this.text().trim() || "This link doesn't have a text, maybe its child is an image";
+                    items.push({
+                        text: text,
+                        link: href
+                    });
+                }
             });
-          }
-        });
 
-        return items;
-      }
+            return items;
+        }
     ]
 });
