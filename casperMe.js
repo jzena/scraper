83f899a9507d5d5ac9bc60babcbdc296f3d20583
Paul Diaz <paulrrdiaz@gmail.com>
2017-06-12 18:35:30 -0500

    Update example

--- casperMe.js
+++ casperMe.js
@@ -8,7 +8,9 @@ var casper = require('casper').create({
   ],
   pageSettings: {
     // iPhone
-    userAgent: "Mozilla/5.0 (iPhone; CPU iPhone OS 5_0 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9A334 Safari/7534.48.3)",
+    userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 5_0 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9A334 Safari/7534.48.3)',
+    // Android
+    //userAgent: 'Mozilla/5.0 (Linux; Android 4.0.4; Galaxy Nexus Build/IMM76B) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.133 Mobile Safari/535.19',
     // Lumia
     //userAgent: 'Mozilla/5.0 (compatible; MSIE 10.0; Windows Phone 8.0; Trident/6.0; IEMobile/10.0; ARM; Touch; NOKIA; Lumia 820)',
   }
@@ -28,7 +30,6 @@ function getLinks() {
 }
 
 casper.start('http://mysites.tracfone.cms.stage.3cinteractive.com/');
-//casper.userAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X)');
 
 casper.waitFor(function check() {
   return this.evaluate(function() {
